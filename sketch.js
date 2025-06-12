621fb3c1e0dd4fb8b37ce0c855d838a7476b080e
djmara <hayatasolo@gmail.com>
2025-06-12 13:17:36 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,66 +4,60 @@ let slices = [];
 let font;
 let quote = " \"That goal became part of soccer history...\nThere are still 10-year-old kids out there today\nwith 'Maradona' on their backs.\" ";
 
-void setup() {
-  size(1280, 720);
-  img = loadImage("maradona.jpg"); 
-  img.resize(width, height);
+function setup() {
+  createCanvas(1280, 720);
+  img = loadImage("maradona.jpg", () => {
+    img.resize(width, height);
+  });
+
   font = createFont("Courier", 30);
 
-quoteLayer = createGraphics(width, height);
-quoteLayer.beginDraw();
-quoteLayer.clear(); // make it transparent instead of black
-quoteLayer.fill(255);
-quoteLayer.textFont(font);
-quoteLayer.textAlign(CENTER, CENTER);
-quoteLayer.text(quote, width / 2, height / 2);
-quoteLayer.endDraw();
-
-  image(img, 0, 0);
+  quoteLayer = createGraphics(width, height);
+  quoteLayer.clear();
+  quoteLayer.fill(255);
+  quoteLayer.textFont(font);
+  quoteLayer.textAlign(CENTER, CENTER);
+  quoteLayer.text(quote, width / 2, height / 2);
 }
 
-void draw() {
+function draw() {
   background(0);
-
-  // base image
   image(img, 0, 0);
 
-  // apply all current slices
-  for (GlitchSlice s : slices) {
-    s.update(); 
+  for (let s of slices) {
+    s.update();
     s.display();
   }
-
-
 }
 
-void triggerGlitchStorm() {
-  for (int i = 0; i < 60; i++) {
-    int h = int(random(8, 30));
-    int y = int(random(height - h));
-    int offset = int(random(-100, 100));  // more violent shift
-    slices.add(new GlitchSlice(y, h, offset));
+function triggerGlitchStorm() {
+  for (let i = 0; i < 60; i++) {
+    let h = floor(random(8, 30));
+    let y = floor(random(height - h));
+    let offset = floor(random(-100, 100));
+    slices.push(new GlitchSlice(y, h, offset));
   }
 }
 
-void keyPressed() {
-  if (key == 'g' || key == 'G') {
+function keyPressed() {
+  if (key === 'g' || key === 'G') {
     triggerGlitchStorm();
   }
 }
 
-void mouseMoved() {
-  for (int i = 0; i < 5; i++) {
-    int h = int(random(5, 20));
-    int y = int(random(height - h));
-    int offset = int(random(-60, 60));  // <-- bidirectional offset
-    slices.add(new GlitchSlice(y, h, offset));
+function mouseMoved() {
+  for (let i = 0; i < 5; i++) {
+    let h = floor(random(5, 20));
+    let y = floor(random(height - h));
+    let offset = floor(random(-60, 60));
+    slices.push(new GlitchSlice(y, h, offset));
   }
 
-  if (slices.size() > 200) {
-    slices.subList(0, slices.size() - 200).clear();
+  if (slices.length > 200) {
+    slices.splice(0, slices.length - 200);
   }
 }
+
 class GlitchSlice {
   constructor(y, h, offset) {
     this.y = y;
@@ -82,3 +76,4 @@ class GlitchSlice {
     copy(quoteLayer, 0, this.y, width, this.h, offsetInt, this.y, width, this.h);
   }
 }
+
